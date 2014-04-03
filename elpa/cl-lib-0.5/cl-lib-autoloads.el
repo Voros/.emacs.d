00cabaacb5d219934f67f3e7f03e2c804500dfdb
David Baerg <vorosdb@gmail.com>
2014-04-03 18:18:46 -0400

    first commit

---
+++ elpa/cl-lib-0.5/cl-lib-autoloads.el
@@ -0,0 +1,25 @@
+;;; cl-lib-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "cl-lib" "cl-lib.el" (21296 54125 0 0))
+;;; Generated autoloads from cl-lib.el
+ (let ((d (file-name-directory #$)))
+  (when (member d load-path)
+    (setq load-path (append (remove d load-path) (list d)))))
+
+;;;***
+
+;;;### (autoloads nil nil ("cl-lib-pkg.el") (21296 54125 291194 0))
+
+;;;***
+
+(provide 'cl-lib-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cl-lib-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,44 @@
+;;C++ indentation
+(setq c-default-style "linux" c-basic-offset 4)
+(setq-default tab-width 4 indent-tabs-mode t)
+
+(add-to-list 'load-path "~/.emacs.d/")
+(load "autopair.el")
+;;(load "auto-pair+.el")
+(require 'autopair)
+(autopair-global-mode)
+;;(require 'autopair+)
+
+;; Function keys
+(global-set-key [f1] 'manual-entry)
+(global-set-key [f2] 'info)
+(global-set-key [f3] 'repeat-complex-command)
+(global-set-key [f4] 'advertised-undo)
+(global-set-key [f5] 'compile)
+(global-set-key [f6] 'buffer-menu)
+(global-set-key [f7] 'other-window)
+(global-set-key [f8] 'find-file)
+(global-set-key [f9] 'save-buffer)
+(global-set-key [f10] 'next-error)
+(global-set-key [f11] 'compile)
+(global-set-key [f12] 'grep)
+
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "M-?") 'mark-paragraph)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+;;(global-set-key (kbd "C-<tab>") 'next-buffer)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
